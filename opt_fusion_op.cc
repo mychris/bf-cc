3c17d69a51e7ed5971a9dbb122a71afae267c0d8
Christoph Göttschkes <christoph.goettschkes@posteo.net>
2024-01-20 22:15:35 +0100

    Fixes fusion optimization.

--- opt_fusion_op.cc
+++ opt_fusion_op.cc
@@ -3,7 +3,10 @@
 
 Instr* OptFusionOp::Run(Instr* op) {
   Instr* head = op;
-  // First loop, fuse the same operations
+  if (op->OpCode() != OpCode::NOP) {
+    head = Instr::Allocate(OpCode::NOP, 0);
+    head->SetNext(op);
+  }
   while (op) {
     OpCode seq_cmd = op->OpCode();
     if (seq_cmd == OpCode::INCR_CELL
@@ -13,7 +16,7 @@ Instr* OptFusionOp::Run(Instr* op) {
       Instr* seq_head = op;
       uintptr_t amount = op->Operand1();
       op = op->Next();
-      while (op->OpCode() == seq_cmd) {
+      while (op && op->OpCode() == seq_cmd) {
         amount += op->Operand1();
         seq_head->SetOperand1(amount);
         seq_head->SetNext(op->Next());
